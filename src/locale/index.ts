07ffbd19c6c0e32eea6fb853d730eabb3cec0703
wcc <308509797@qq.com>
2023-01-29 16:06:12 +0800

    feat: 优化国际化配置

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,19 +1,24 @@
 import { createI18n } from 'vue-i18n'
 
-import en from "./en-US"
-import zh from "./zh-CN"
-
-
 const defaultLocale = localStorage.getItem('locale') || 'en-US'
- 
+
+const messages = Object.fromEntries(
+  Object.entries(
+    // https://cn.vitejs.dev/guide/features.html#glob-import
+    // vue3中使用 import.meta.glob 来引入多个不同的文件
+    // vue2中是使用 require.context
+    import.meta.glob<{ default: any}>('./*.y(a)?ml', { eager: true })
+  ).map(([key, value]) => {
+    const yaml = key.endsWith('.yaml')
+    return [key.slice(14, yaml ? -5 : -4), value.default]
+  })
+)
+
 const i18n = createI18n({
   locale: defaultLocale,
   fallbackLocale: 'en-US',
   legacy: false,
-  messages: {
-    'en-US': en,
-    'zh-CN': zh
-  },
+  messages,
 })
 
 export default i18n
