85fe80ca4d6c4024a08f057441af23bae1352e1b
wcc <308509797@qq.com>
2023-02-14 10:17:05 +0800

    feat: 优化页面标题

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+// 配置页面标题
+const title = import.meta.env.VITE_TITLE
+onMounted(() => {
+  document.title = title ?? 'ichstudy'
+})
 </script>
 
 <template>
