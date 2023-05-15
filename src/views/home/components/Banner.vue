acfc178a7e8abc625bfb126c4079d15bc041865c
wcc <308509797@qq.com>
2023-05-15 13:30:08 +0800

    feat: 添加首页banner文字反色效果

--- src/views/home/components/Banner.vue
+++ src/views/home/components/Banner.vue
@@ -11,47 +11,70 @@ const startBtn = () => {
 </script>
 
 <template>
-  <div class="f-c-c w-full h-screen">
-    <div class="content w-60% pr-20">
+  <div class="banner f-c-c w-full h-screen">
+    <div class="content">
       <div class="title text-8xl font-600 my-4">
         {{ $t('context.title') }}
       </div>
-      <p class="text-lg my-8">
+      <div class="w-60% text-lg my-8">
         {{ $t('context.subtitle') }}
-      </p>
+      </div>
       <button class="btn text-lg px-10" @click="startBtn()">
         {{ $t('context.start') }}
       </button>
-      <div class="mask">
+      <div class="ink">
         <!-- <img src="src/assets/ink.png" alt=""> -->
         <img v-show="!isDark" src="src/assets/ink-black.png" alt="">
         <img v-show="isDark" src="src/assets/ink-white.png" alt="">
       </div>
     </div>
+    <div class="content mask">
+      <div class="title text-8xl font-600 my-4">
+        {{ $t('context.title') }}
+      </div>
+      <div class="w-60% text-lg my-8">
+        {{ $t('context.subtitle') }}
+      </div>
+      <button class="btn text-lg px-10" @click="startBtn()">
+        {{ $t('context.start') }}
+      </button>
+    </div>
   </div>
 </template>
 
 <style scoped lang="less">
-.content {
+.banner {
   position: relative;
+}
+.content {
+  width: 60%;
+  padding-right: 50px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .title {
   font-family: Montserrat;
 }
 
-.mask {
+.ink {
   position: absolute;
-  right: -50px;
+  right: 0px;
   top: 50%;
   transform: translateY(-50%);
   z-index: -1;
+}
 
-  img {
-    -webkit-mask-image: linear-gradient(#000, #000);;
-    mask-image: linear-gradient(#000, #000);;
-    mask-repeat: no-repeat;
-    mask-position: center;
-  }
+.content.mask {
+  color: @text-dark;
+
+  mask-image: url('@/assets/ink-mask.png');
+  mask-repeat: no-repeat;
+  mask-position: center;
+  -webkit-mask-image: url('@/assets/ink-mask.png');
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-position: center right;
 }
 </style>
